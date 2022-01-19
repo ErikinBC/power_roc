85f05886a279e80141bff809f2f54b38c641e367
ErikinBC <alla_hack@hotmail.com>
2022-01-19 17:44:13 -0500

    studentized power coverage; working decently

--- funs_stats.py
+++ funs_stats.py
@@ -11,18 +11,13 @@ def auc_rank(y, s):
     num = sum(rankdata(s)[y == 1]) - n1*(n1+1)/2
     auc = num / den
     return auc
-    # n1n0=False
-    # if n1n0:
-    #     return auc, n1, n0
 
 # SKLearn wrapper to calculate empirical ROC
 def emp_roc_curve(y, s):
-    fpr, tpr, thresh = sk_roc_curve(y, s)
-    res = pd.DataFrame({'thresh':thresh, 'tpr':tpr, 'fpr':fpr})
+    fpr, sens, thresh = sk_roc_curve(y, s)
+    spec = 1-fpr
+    res = pd.DataFrame({'thresh':thresh, 'sens':sens, 'spec':spec})
     return res
-
-
-
 
 # mu=1;p=0.1;n_points=500;ptail=1e-3
 class dgp_bin():
@@ -30,21 +25,41 @@ class dgp_bin():
     mu:     Mean of positive class N(mu, 1)
     p:      Probability of an observation being a positive class
     """
-    def __init__(self, mu, p):
+    def __init__(self, mu, p, thresh=None, sens=None, spec=None):
         assert mu > 0, 'Mean needs to be greater than zero'
         self.mu = mu
         assert (p > 0) and (p < 1), 'p needs to be between 0 and 1'
         self.p = p
         # Calculate ground truth AUROC
-        self.auroc = norm.cdf(mu/np.sqrt(2))
+        self.auroc_oracle = norm.cdf(mu/np.sqrt(2))
+        # Calculate the oracle values
+        self.thresh, self.sens, self.spec = thresh, sens, spec
+        check_none = (thresh is not None) or (sens is not None) or (spec is not None)
+        assert check_none, 'at least one field needs to be assigned: thresh, sens, or spec'
+        if thresh is not None:
+            self.thresh_oracle = thresh
+        if sens is not None:
+            self.thresh_oracle = norm.ppf(1-sens) + self.mu
+        if spec is not None:
+            self.thresh_oracle = norm.ppf(spec)
+        
+
+    def thresh2sens(self, thresh):
+        sens = 1 - norm.cdf(thresh - self.mu)
+        return sens
+
+    def thresh2spec(self, thresh):
+        spec = norm.cdf(thresh)
+        return spec
 
     def roc_curve(self, n_points=500, ptail=1e-3):
+        # Generate sequence of scores within distribution of 0 and 1 class
         s_lower = norm.ppf(ptail)
         s_upper = norm.ppf(1-ptail) + self.mu
         s_seq = np.linspace(s_lower, s_upper, n_points)
-        tpr = 1 - norm.cdf(s_seq - self.mu)
-        fpr = 1 - norm.cdf(s_seq)
-        res = pd.DataFrame({'thresh':s_seq, 'tpr':tpr, 'fpr':fpr})
+        sens = self.thresh2sens(s_seq)
+        spec = self.thresh2spec(s_seq)
+        res = pd.DataFrame({'thresh':s_seq, 'sens':sens, 'spec':spec})
         return res
 
     def dgp_bin(self, n, seed=None):
@@ -62,5 +77,137 @@ class dgp_bin():
         res = pd.DataFrame({'y':labels, 's':scores})
         return res
 
+    """
+    Calculate the threshold based on empirical data
+    If threshold was already supplied, empirical == oracle threshold
+    If sens or spec was supplied, empirical != oracle threshol
+    """
+    def learn_threshold(self, y, s):
+        if self.thresh is not None:
+            self.thresh_emp = self.thresh_oracle
+        if self.sens is not None:
+            s1 = s[y == 1]
+            self.thresh_emp = np.quantile(s1, 1-self.sens)
+        if self.spec is not None:
+            s0 = s[y == 0]
+            self.thresh_emp = np.quantile(s0, self.spec)
+        # Assign the empirical sensitivity and specificity
+        self.sens_emp, self.spec_emp = self.get_tptn(y, s).values.flat
+        # Get the oracle sensitivity and specificity based on the learned threshold
+        self.sens_oracle = self.thresh2sens(self.thresh_emp)
+        self.spec_oracle = self.thresh2spec(self.thresh_emp)
+        # Save labels and score for later
+        self.y_thresh = np.array(y)
+        self.s_thresh = np.array(s)
+        
+    def predict(self, s):
+        yhat = np.where(s >= self.thresh_emp, 1, 0)
+        return yhat
 
+    def get_tptn(self, y, s):
+        yhat = self.predict(s)
+        sens = np.mean(yhat[y == 1] == 1)
+        spec = np.mean(yhat[y == 0] == 0)
+        res = pd.DataFrame({'sens':sens, 'spec':spec}, index=[0])
+        return res
+
+    def create_df(self):
+        msr = np.repeat(['sens', 'spec', 'thresh'], 2)
+        tt = np.tile(['oracle','emp'],3)
+        val = [self.sens_oracle, self.sens_emp, self.spec_oracle, self.spec_emp, self.thresh_oracle, self.thresh_emp]
+        self.df_rv = pd.DataFrame({'msr':msr, 'tt':tt, 'val':val})
+
+    """
+    Function to calculate power of a binomial proportion for a 1-sided test
+
+    p_act:                  True proportion
+    p_null:                 Null hypothesis proportion
+    n:                      Sample size
+    alpha:                  Type-I error rate
+    """
+    @staticmethod
+    def power_binom(p_act, p_null, n, alpha=0.05):
+        assert (alpha > 0) and (alpha < 1), 'Type-I error must be between 0 to 1'
+        t_alpha = norm.ppf(1-alpha)
+        sigma_act = np.sqrt(p_act*(1-p_act)/n)
+        sigma_null = np.sqrt(p_null*(1-p_null)/n)
+        p_delta = p_act - p_null
+        power = 1 - norm.cdf( (sigma_null*t_alpha - p_delta)/sigma_null )
+        return power
+
+
+    """
+    Carry out the studentized bootstrapped on the test-set scores to determine 95% CI for sens/spec
+    Because power is a monotonic function of sens/spec, these bounds can be used to estimate power
+    http://users.stat.umn.edu/~helwig/notes/bootci-Notes.pdf
+    """
+    def run_bootstrap(self, s1, s0, alpha, n_bs=1000, n_student=None, seed=None):
+        assert (s1 is not None) or (s0 is not None), 'Specify either s0 or s1'
+        if n_student is None:
+            n_student = n_bs
+        alpha_vec = np.array([1-alpha/2, alpha/2])
+        # (i) Bootstrap distribution
+        n1, n0 = len(s1), len(s0)
+        s1_bs = s1.sample(frac=n_bs,replace=True,random_state=seed).values.reshape([n_bs, n1])
+        s0_bs = s0.sample(frac=n_bs,replace=True,random_state=seed).values.reshape([n_bs, n0])
+        sens_bs = np.mean(s1_bs >= self.thresh_emp, 1)
+        spec_bs = np.mean(s0_bs < self.thresh_emp, 1)
+        sens_se = sens_bs.std(ddof=1)
+        spec_se = spec_bs.std(ddof=1)
+
+        # (ii) Bootstrap the bootstrapped samples to get a variance of each observation
+        sens_se_student, spec_se_student = np.zeros(n_bs), np.zeros(n_bs)
+        for j in range(n_bs):
+            # Studentized score distribution
+            s1_student = pd.Series(s1_bs[j]).sample(frac=n_student,replace=True,random_state=j).values.reshape([n_student, n1])
+            sens_student = np.mean(s1_student >= self.thresh_emp, 1)
+            sens_se_student[j] = sens_student.std(ddof=1)
+            s0_student = pd.Series(s0_bs[j]).sample(frac=n_student,replace=True,random_state=j).values.reshape([n_student, n0])
+            spec_student = np.mean(s0_student >= self.thresh_emp, 1)
+            spec_se_student[j] = spec_student.std(ddof=1)
+        t_sens = (sens_bs - self.sens_emp) / sens_se_student
+        t_spec = (spec_bs - self.spec_emp) / spec_se_student
+
+        # Find the alpha-quantile using the studentized formula
+        t_alpha_sens = np.quantile(t_sens, alpha_vec)
+        t_alpha_spec = np.quantile(t_spec, alpha_vec)
+
+        # (iii) Get the confidence intervals
+        sens_ci = self.sens_emp - t_alpha_sens*sens_se
+        spec_ci = self.spec_emp - t_alpha_spec*spec_se
+        res_sens = pd.DataFrame({'msr':'sens', 'tt':['lb','ub'], 'val':sens_ci})
+        res_spec = pd.DataFrame({'msr':'spec', 'tt':['lb','ub'], 'val':spec_ci})
+        res = pd.concat(objs=[res_sens, res_spec], axis=0).reset_index(drop=True)
+        return res
+        
+
+    """
+    null_sens:              Sensitivity null hypothesis to be rejected during silent period
+    null_spec:              Specificity null hypothesis to be rejected during silent period
+    n1_trail:               Number of positive samples during trial
+    n0_trial:               Number of negative samples during trial
+    """
+    def run_power(self, null_sens, null_spec, n1_trial, n0_trial, alpha=0.05, n_bs=1000, seed=1):
+        if null_sens > self.sens_emp:
+            print('Warning! Null hypothesis for sensitivity should be less than empirical sensitivity')
+        if null_spec > self.spec_emp:
+            print('Warning! Null hypothesis for specificity should be less than empirical sensitivity')
+        assert isinstance(n1_trial, int) and n1_trial > 0
+        assert isinstance(n0_trial, int) and n0_trial > 0
+        s1 = pd.Series(self.s_thresh[self.y_thresh == 1])
+        s0 = pd.Series(self.s_thresh[self.y_thresh == 0])
+        # Generate bootstrapped range of empirical sens/spec
+        perf_ci = self.run_bootstrap(s1, s0, alpha, n_bs=n_bs, n_student=n_bs, seed=seed)
+        # Merge on null and sample size
+        lst_msr = ['sens','spec']
+        lst_n = [n1_trial, n0_trial]
+        lst_h0 = [null_sens, null_spec]
+        dat_n = pd.DataFrame({'msr':lst_msr,'n':lst_n, 'h0':lst_h0})
+        self.power_ci = perf_ci.merge(dat_n)
+        self.power_ci = self.power_ci.assign(power=lambda x: self.power_binom(p_act=x['val'], p_null=x['h0'], n=x['n'], alpha=alpha))
+        # Determine the "true" power
+        power_sens = self.power_binom(p_act=self.sens_oracle, p_null=null_sens, n=n1_trial, alpha=alpha)
+        power_spec = self.power_binom(p_act=self.spec_oracle, p_null=null_spec, n=n0_trial, alpha=alpha)
+        tmp_df = pd.DataFrame({'msr':lst_msr, 'power':[power_sens, power_spec]})
+        self.df_power = tmp_df.merge(self.power_ci.pivot('msr','tt','power').reset_index())
 
--- run_sims.py
+++ run_sims.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import plotnine as pn
+from time import time
 from funs_stats import dgp_bin, emp_roc_curve, auc_rank
 from funs_support import makeifnot
 
@@ -9,38 +10,122 @@ dir_base = os.getcwd()
 dir_figures = os.path.join(dir_base, 'figures')
 makeifnot(dir_figures)
 
+
+################################
+# --- (3) POWER ESTIMATES  --- #
+
+mu, p = 1, 0.5
+n_train, n_trial = 400, 400
+n_bs, alpha = 1000, 0.05
+sens = 0.5
+margin = 0.1
+enc_dgp = dgp_bin(mu=mu, p=p, sens=sens)
+
+# (i) SET UP THE TEST STATISTIC AND REJECT CRITERION
+# (ii) BOOSTRAP THE SCORES, SENSITIVIES, AND ESTIMATE POWER RANGE BASED ON THAT
+i = 0
+n1_trial, n0_trial = 200, 200
+self = dgp_bin(mu=mu, p=p, sens=sens)
+dat_train = enc_dgp.dgp_bin(n=n_train, seed=i)
+self.learn_threshold(y=dat_train['y'], s=dat_train['s'])
+enc_dgp.create_df()
+null_sens = self.sens_emp - margin
+null_spec = self.spec_emp - margin
+self.run_power(null_sens, null_spec, n1_trial, n0_trial, alpha, n_bs, seed=i+2)
+
+nsim = 250
+stime = time()
+holder_power_ci = []
+for i in range(nsim):
+    # (i) Generate data (going to to use oracle n1, n0)
+    dat_train = enc_dgp.dgp_bin(n=n_train, seed=i)
+    dat_trial = enc_dgp.dgp_bin(n=n_trial, seed=i+1)
+    n0_trial, n1_trial = dat_trial['y'].value_counts().sort_index()
+
+    # (ii) Learn threshold on sensitivity target and estimate power
+    enc_dgp.learn_threshold(y=dat_train['y'], s=dat_train['s'])
+    enc_dgp.create_df()
+    null_sens = enc_dgp.sens_emp - margin
+    null_spec = enc_dgp.spec_emp - margin
+    enc_dgp.run_power(null_sens, null_spec, n1_trial, n0_trial, alpha, n_bs, seed=i+2)
+    holder_power_ci.append(enc_dgp.df_power.assign(sim=i))
+    
+    # (iii) Run hypothesis test
+
+    # Run-time update
+    if (i + 1) % 1 == 0:
+        dtime, n_left = time() - stime, nsim-(i+1)
+        srate = (i+1)/dtime
+        seta = n_left / srate
+        print('Iteration %i of %i (ETA: %i seconds, %0.1f minutes)' % (i+1, nsim, seta, seta/60))
+
+# Compare power coverage
+res_power = pd.concat(holder_power_ci).reset_index(drop=True)
+res_power = res_power.assign(cover=lambda x: (x['power']>x['lb']) & (x['power']<x['ub']) )
+res_power.groupby(['msr'])['cover'].mean().reset_index()
+
+
 ##############################################
 # --- (1) CHECK EMPIRICAL TO GROUNDTRUTH --- #
 
-enc_dgp = dgp_bin(mu=1, p=0.5)
-nsim = 250
-n = 100
+nsim, n = 250, 100
+mu, p = 1, 0.5
+enc_dgp = dgp_bin(mu=mu, p=p, thresh=mu)
 holder_auc = np.zeros(nsim)
 holder_roc = []
 for i in range(nsim):
     dat_i = enc_dgp.dgp_bin(n=n, seed=i)
     # (i) Empirical AUROC
-    holder_auc[i] = auc_rank(dat_i.y, dat_i.s)
+    holder_auc[i] = auc_rank(dat_i['y'], dat_i['s'])
     # (ii) Empirical ROC curve
-    holder_roc.append(emp_roc_curve(dat_i.y, dat_i.s).assign(sim=i))
-
+    tmp_roc = emp_roc_curve(dat_i['y'], dat_i['s']).assign(sim=i)
+    holder_roc.append(tmp_roc)
 emp_roc = pd.concat(holder_roc).reset_index(drop=True)
 gt_roc = enc_dgp.roc_curve()
 df_roc = pd.concat(objs=[emp_roc, gt_roc.assign(sim=-1)])
 df_roc = df_roc.assign(tt=lambda x: np.where(x['sim']==-1,'Ground Truth','Simulation'))
-df_auc = pd.DataFrame({'auc':holder_auc,'gt':enc_dgp.auroc})
+df_auc = pd.DataFrame({'auc':holder_auc,'gt':enc_dgp.auroc_oracle})
 
 
-##############################################
-# --- (2)  --- #
+###################################
+# --- (2) SENSITIVITY TARGET  --- #
+
+di_msr = {'sens':'Sensitivity', 'spec':'Specificity', 'thresh':'Threshold'}
+di_tt = {'oracle':'Oracle', 'emp':'Empirical', 'test':'Out-of-sample'}
+nsim = 250
+mu, p = 1, 0.5
+n_train, n_test = 200, 100000
+sens = 0.5
+enc_dgp = dgp_bin(mu=mu, p=p, sens=sens)
+
+holder = []
+for i in range(nsim):
+    dat_train = enc_dgp.dgp_bin(n=n, seed=i)
+    enc_dgp.learn_threshold(y=dat_train['y'], s=dat_train['s'])
+    enc_dgp.create_df()
+    dat_test = enc_dgp.dgp_bin(n=n, seed=i+1)
+    res_test = enc_dgp.get_tptn(dat_test['y'], dat_test['s'])
+    res_test = res_test.assign(tt='test').melt('tt',None,'msr','val')
+    tmp_df = pd.concat(objs=[enc_dgp.df_rv, res_test], axis=0)
+    tmp_df.insert(0, 'sim', i)
+    holder.append(tmp_df)
+res_tfpr = pd.concat(holder).reset_index(drop=True)
+res_tfpr['msr'] = res_tfpr['msr'].map(di_msr)
+res_tfpr['tt'] = res_tfpr['tt'].map(di_tt)
+res_tfpr_wide = res_tfpr.pivot_table('val',['sim','msr'],'tt').reset_index()
+res_tfpr_wide = res_tfpr_wide.melt(['sim','msr',di_tt['oracle']])
+res_tfpr_wide = res_tfpr_wide.dropna().reset_index(drop=True)
+
+
+
 
 
 #######################
 # --- (X) FIGURES --- #
 
 # (i) Empirical ROC to actual
-gg_roc_gt = (pn.ggplot(df_roc,pn.aes(x='fpr',y='tpr',size='tt',color='tt',alpha='tt',group='sim')) + 
-    pn.theme_bw() + pn.labs(x='1-FPR',y='TPR') + 
+gg_roc_gt = (pn.ggplot(df_roc,pn.aes(x='1-spec',y='sens',size='tt',color='tt',alpha='tt',group='sim')) + 
+    pn.theme_bw() + pn.labs(x='1-Specificity',y='Sensitivity') + 
     pn.ggtitle('ROC Curve (Ground Truth vs Simulation)') + 
     pn.geom_step() + 
     pn.theme(legend_position=(0.7,0.3)) + 
@@ -56,3 +141,19 @@ gg_auc_gt = (pn.ggplot(df_auc,pn.aes(x='auc')) + pn.theme_bw() +
     pn.labs(x='AUROC',y='Frequency') + 
     pn.ggtitle('Black lines show ground truth AUROC'))
 gg_auc_gt.save(os.path.join(dir_figures,'gg_auc_gt.png'),height=4,width=6)
+
+# (iii) Oracle vs OOS vs Empirical
+gg_sens_scatter = (pn.ggplot(res_tfpr_wide, pn.aes(x='value',y='Oracle',color='tt')) + 
+    pn.theme_bw() + pn.scale_color_discrete(name=' ') + 
+    pn.labs(x='Observed',y='Oracle') + 
+    pn.geom_point() + pn.geom_abline(slope=1,intercept=0,linetype='--') + 
+    pn.facet_wrap('~msr'))
+gg_sens_scatter.save(os.path.join(dir_figures,'gg_sens_scatter.png'),height=4,width=12)
+
+gg_sens_hist = (pn.ggplot(res_tfpr, pn.aes(x='val',fill='tt')) + 
+    pn.theme_bw() + pn.scale_fill_discrete(name=' ') + 
+    pn.labs(x='Value',y='Frequency') + 
+    pn.geom_histogram(alpha=0.5,color='grey',bins=30,position='identity') + 
+    pn.facet_wrap('~msr',scales='free_x'))
+gg_sens_hist.save(os.path.join(dir_figures,'gg_sens_hist.png'),height=4,width=12)
+
