836b729a8d03612608dea42f7b064b8397afaeb2
ErikinBC <alla_hack@hotmail.com>
2022-01-21 09:04:27 -0500

    statistical results updated

--- run_rwd.py
+++ run_rwd.py
@@ -17,6 +17,7 @@ nsim = 250
 alpha = 0.05
 di_msr = {'sens':'Sensitivity', 'spec':'Specificity', 'thresh':'Threshold'}
 di_ds = {'test':'Test','hsk':'HSK', 'stan':'Stanford', 'iowa':'Iowa'}
+di_metric = {'pval':'P-value', 'stat':'Statistic', 'z':'Z-score'}
 lst_msr = ['sens','spec']
 
 #########################
@@ -184,6 +185,7 @@ margin_sens = 0.25
 margin_spec = 0.05
 null_sens = enc_test.sens_emp - margin_sens
 null_spec = enc_test.spec_emp - margin_spec
+vec_target = np.array([enc_test.sens_emp, enc_test.spec_emp])  
 vec_null = np.array([null_sens, null_spec])
 enc_test.set_null_hypotheis(null_sens, null_spec)
 
@@ -191,7 +193,8 @@ enc_test.set_null_hypotheis(null_sens, null_spec)
 lst_alpha = [alpha/2,0.5,1-alpha/2]
 di_alpha = dict(zip(lst_alpha, ['lb','med','ub']))
 
-# CREATE AN ARTIFICIAL TIME SERIES....
+# Simulated the range of sensitivity/specificities through random patient order
+nsim = 100
 lst_ds = ['stan', 'iowa', 'hsk']
 holder_ds = []
 for ds in lst_ds:
@@ -199,41 +202,81 @@ for ds in lst_ds:
     dat_ds = dat_trial.query('ds == @ds')
     n0_ds, n1_ds = dat_ds['y'].value_counts().sort_index().values
     n0_ds, n1_ds = int(n0_ds), int(n1_ds)
-    
-    # (i) Calculate the power expectation 
-    enc_test.run_power(n1_trial=n1_ds, n0_trial=n0_ds, method='quantile')
-    tmp_ds = enc_test.df_power.drop(columns=['power','method'])
-    # (ii) Run the statistical test over a simulated silent trial
+    # Run the statistical test over a simulated silent trial
     lst_frac = np.arange(0.25, 1.01, 0.05).round(2)
-    holder_sim = []
     for j in range(nsim):
         if (j+1) % 10 == 0:
             print('Simulation %i of %i' % (j+1, nsim))
-        dat_ds_rand = dat_ds.groupby('y').sample(frac=1,random_state=j,replace=False)
+        # Shuffle the rows of the data
+        dat_ds_rand = dat_ds.sample(frac=1,random_state=j,replace=False)
+        # Index the patients (groupby label to preserve balance)
         dat_ds_rand['idx'] = dat_ds_rand.groupby('y').cumcount()+1
         dat_ds_rand = dat_ds_rand.reset_index(drop=True).rename_axis('idt').reset_index()
         dat_ds_rand = dat_ds_rand.merge(df_n_ds).assign(idx=lambda x: x['idx']/x['n'])
         for frac in lst_frac:
             dat_ds_frac = dat_ds_rand.query('idx <= @frac')
             n0_frac, n1_frac = dat_ds_frac['y'].value_counts().sort_index().values
-            tmp_test = enc_test.get_tptn(dat_ds_frac['y'], dat_ds_frac['s'])
-            p_act_frac = tmp_test.values.flatten()
+            n0_frac, n1_frac = int(n0_frac), int(n1_frac)
+            # (i) Calculate expected power at this sample size
+            enc_test.run_power(n1_trial=n1_frac, n0_trial=n0_frac, method='quantile')
+            tmp_power_frac = enc_test.df_power.drop(columns=['power','method','h0'])
+            # (ii) Calculate actual test statistics
+            p_act_frac = enc_test.get_tptn(dat_ds_frac['y'], dat_ds_frac['s'])
+            p_act_frac = p_act_frac.values.flatten()
             n_vec_frac = np.array([n1_frac, n0_frac])
-            res_frac_stat = enc_test.binom_stat(p_act=p_act_frac, p_null=vec_null, n=n_vec_frac)
-            res_frac_stat.insert(0, 'msr', lst_msr)
-            res_frac_stat = res_frac_stat.assign(val=p_act_frac, frac=frac, sim=j)
-            holder_sim.append(res_frac_stat)
-    # Merge and clean
-    cn_gg = ['msr','frac']
-    cn_val = ['pval','val']
-    res_sim = pd.concat(holder_sim).reset_index(drop=True)
-    res_sim = res_sim.groupby(cn_gg)[cn_val].quantile(lst_alpha).reset_index()
-    res_sim.rename(columns={'level_'+str(len(cn_gg)):'tt'}, inplace=True)
-    res_sim = res_sim.melt(cn_gg+['tt'],None,'metric')
-    res_sim = res_sim.pivot_table('value',cn_gg+['metric'],'tt').reset_index()
-    res_sim.rename(columns=di_alpha, inplace=True)
-    res_sim.insert(0, 'ds', ds)
-    holder_ds.append(res_sim)
+            tmp_frac_stat = enc_test.binom_stat(p_act=p_act_frac, p_null=vec_null, n=n_vec_frac)
+            tmp_frac_stat = tmp_frac_stat.assign(msr=lst_msr).drop(columns='reject')
+            tmp_frac_stat = tmp_frac_stat.assign(stat=p_act_frac, frac=frac, sim=j, ds=ds)
+            # (iii) Merge and store
+            tmp_frac_stat = tmp_frac_stat.merge(tmp_power_frac)
+            holder_ds.append(tmp_frac_stat)
+# Merge and clean
+res_sim_trial = pd.concat(holder_ds).reset_index(drop=True)
+cn_gg = ['ds','msr','frac','n_trial','lb','ub']
+cn_val = list(res_sim_trial.columns.drop(cn_gg))
+assert np.all(res_sim_trial.groupby(cn_gg).size() == nsim)
+# Calculate the 95% range across fractions
+res_ci_trial = res_sim_trial.groupby(cn_gg)[cn_val].quantile(lst_alpha).reset_index()
+res_ci_trial.rename(columns={'level_%s'%len(cn_gg):'tt'},inplace=True)
+# To avoid confusion, make lower and upper bound estimates of power into type-2 errors
+res_ci_trial = res_ci_trial.assign(beta_lb=lambda x: 1-x['ub'], beta_ub=lambda x: 1-x['lb'])
+res_ci_trial.drop(columns=['lb','ub'],inplace=True)
+cn_gg = res_ci_trial.columns.drop(cn_val)
+res_ci_trial = res_ci_trial.melt(cn_gg,cn_val,'metric','val')
+# Get empirical CI around simulated performance
+res_ci_trial = res_ci_trial.pivot_table('val',list(cn_gg.drop('tt'))+['metric'],'tt')
+res_ci_trial = res_ci_trial.rename(columns=di_alpha).reset_index()
+# Remove sim and z-score for now
+res_ci_trial = res_ci_trial[~res_ci_trial['metric'].isin(['z','sim'])].reset_index(drop=True)
+res_ci_trial['metric'] = res_ci_trial['metric'].map(di_metric)
 
-# 
-# dat_test
+# Horizontal lines to show null
+df_null = pd.DataFrame({'msr':lst_msr, 'null':vec_null, 'target':vec_target})
+df_null = df_null.melt('msr',None,'calib')
+
+# (i) Plot trial results
+gtit = 'Horizontal lines shows null hypothesis\nLine range shows values of simulations'
+# Put beta as a type of metric
+grp1 = ['ds','msr','frac','n_trial']
+tmp1 = res_ci_trial.melt(grp1,['beta_lb','beta_ub'])
+tmp1 = tmp1.drop_duplicates().assign(metric='Type-II error')
+tmp1['tt'] = tmp1['tt'].str.replace('beta_','',regex=False)
+tmp1 = tmp1.pivot_table('value',grp1+['metric'],'tt').reset_index()
+tmp2 = res_ci_trial.drop(columns=['beta_lb','beta_ub'])
+tmp_df = pd.concat(objs=[tmp1, tmp2],axis=0)
+
+colz = ["#F8766D","#619CFF","#00BA38"]
+gg_trial_sim = (pn.ggplot(tmp_df,pn.aes(x='frac',y='med',color='metric',fill='metric')) + 
+    pn.geom_line() + 
+    pn.geom_ribbon(pn.aes(ymin='lb',ymax='ub'),alpha=0.5,color='black') + 
+    pn.theme_bw() + pn.ggtitle(gtit) + 
+    pn.labs(x='Fraction of patients',y='Value') + 
+    pn.theme(legend_position=(0.5,0.0), legend_direction='horizontal', legend_box='horizontal') +
+    pn.scale_color_manual(name='Measure',values=colz) + 
+    pn.scale_fill_manual(name='Measure',values=colz) + 
+    pn.geom_hline(pn.aes(yintercept='value',linetype='calib'),data=df_null) + 
+    pn.scale_linetype_discrete(name='Target', labels=['Null (H0)','Calibrated']) + 
+    pn.facet_grid('msr~ds',labeller=pn.labeller(msr=di_msr, ds=di_ds)))
+gg_trial_sim.save(os.path.join(dir_figures,'gg_trial_sim.png'),height=7,width=12)
+
+
